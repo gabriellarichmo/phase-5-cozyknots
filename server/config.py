a0213ce9edd10a998d48570dc202899e437e7811
Gabriella Richmond <gabriella.m.richmond@gmail.com>
2024-04-24 16:13:29 -0700

    not sure

--- server/config.py
+++ server/config.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+# from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 from sqlalchemy import MetaData
